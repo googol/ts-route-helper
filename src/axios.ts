e53d8ee37ce631989f5799a98d45c236446becd9
Miika Hänninen <miika.hanninen@reaktor.com>
2017-08-23 17:00:07 +0300

    Extend axios helper for post, put & delete

--- src/axios.ts
+++ src/axios.ts
@@ -1,9 +1,13 @@
 import * as A from 'axios';
-import { GetRoute, HttpResult, UrlParams, GenericInternalServerError } from './route';
+import { GetRoute, PostRoute, PutRoute, DeleteRoute, HttpResult, UrlParams, GenericInternalServerError } from './route';
 import * as Bluebird from 'bluebird';
 
-function requestRoute<RouteParams extends string, ResultType extends HttpResult>(axios: A.AxiosInstance, route: GetRoute<RouteParams, ResultType>, routeParams: UrlParams<RouteParams>): Bluebird<ResultType | GenericInternalServerError> {
-    return Bluebird.try(() => axios.get(route.getUrl(routeParams), { validateStatus: () => true }))
+function requestRoute<RouteParams extends string, ResultType extends HttpResult>(axios: A.AxiosInstance, route: GetRoute<RouteParams, ResultType>, routeParams: UrlParams<RouteParams>): Bluebird<ResultType | GenericInternalServerError>;
+function requestRoute<RouteParams extends string, ResultType extends HttpResult, BodyType>(axios: A.AxiosInstance, route: PostRoute<RouteParams, ResultType, BodyType>, routeParams: UrlParams<RouteParams>, body: BodyType): Bluebird<ResultType | GenericInternalServerError>;
+function requestRoute<RouteParams extends string, ResultType extends HttpResult, BodyType>(axios: A.AxiosInstance, route: PutRoute<RouteParams, ResultType, BodyType>, routeParams: UrlParams<RouteParams>, body: BodyType): Bluebird<ResultType | GenericInternalServerError>;
+function requestRoute<RouteParams extends string, ResultType extends HttpResult, BodyType>(axios: A.AxiosInstance, route: DeleteRoute<RouteParams, ResultType, BodyType>, routeParams: UrlParams<RouteParams>, body: BodyType): Bluebird<ResultType | GenericInternalServerError>;
+function requestRoute(axios: A.AxiosInstance, route: any, routeParams: any, body?: any): any {
+    return Bluebird.try(() => axios.request({ url: route.getUrl(routeParams), method: route.method, data: body, validateStatus: () => true }))
         .then((axiosResponse) => {
             if (axiosResponse.status === 500 && typeof axiosResponse.data === 'object' && typeof axiosResponse.data.errorMessage === 'string') {
                 const response: GenericInternalServerError = {
